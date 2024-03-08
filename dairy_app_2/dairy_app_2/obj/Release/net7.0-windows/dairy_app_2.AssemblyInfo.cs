03cb06da777e33f04ab601caff20f2aaf4983db9
Pratik Aryal <aryalpatrick@gmail.com>
2024-03-09 04:04:53 +0545

    inserted images in button

--- dairy_app_2/dairy_app_2/obj/Release/net7.0-windows/dairy_app_2.AssemblyInfo.cs
+++ dairy_app_2/dairy_app_2/obj/Release/net7.0-windows/dairy_app_2.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("dairy_app_2")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66cdfa60ad4dde2898eacc995d04dd2e9de68adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("dairy_app_2")]
