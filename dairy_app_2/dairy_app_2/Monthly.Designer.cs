0f2a977b59a6fafa0b034fe32bd25bb8b1cd6f92
Pratik Aryal <aryalpatrick@gmail.com>
2024-06-11 16:23:52 +0545

    update

--- dairy_app_2/dairy_app_2/Monthly.Designer.cs
+++ dairy_app_2/dairy_app_2/Monthly.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label6 = new Label();
+            dateTimePicker2 = new DateTimePicker();
             SuspendLayout();
             // 
             // label1
@@ -37,17 +41,57 @@
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
             label1.Location = new Point(21, 100);
             label1.Name = "label1";
-            label1.Size = new Size(90, 32);
+            label1.Size = new Size(118, 32);
             label1.TabIndex = 0;
-            label1.Text = "Mnthly";
+            label1.Text = "All Report";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.ControlDarkDark;
+            label5.Location = new Point(21, 183);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 32);
+            label5.TabIndex = 9;
+            label5.Text = "From:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(106, 189);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.ControlDarkDark;
+            label6.Location = new Point(428, 183);
+            label6.Name = "label6";
+            label6.Size = new Size(44, 32);
+            label6.TabIndex = 11;
+            label6.Text = "To:";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(350, 239);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 12;
             // 
             // Monthly
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label5);
             Controls.Add(label1);
             Name = "Monthly";
-            Size = new Size(600, 500);
+            Size = new Size(900, 500);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +99,9 @@
         #endregion
 
         private Label label1;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private Label label6;
+        private DateTimePicker dateTimePicker2;
     }
 }
