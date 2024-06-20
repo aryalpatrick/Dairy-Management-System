70d99750684ca2119ac63ee2592c2aa52dc28886
Pratik Aryal <aryalpatrick@gmail.com>
2024-06-20 22:48:30 +0545

    update

--- dairy_app_2/dairy_app_2/obj/Debug/net7.0-windows/dairy_app_2.AssemblyInfo.cs
+++ dairy_app_2/dairy_app_2/obj/Debug/net7.0-windows/dairy_app_2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dairy_app_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b71967c04070812fb510fae262c1a15241e4e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d3dd5379ff4cf85ed0f3591adf3aba08a50d2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("dairy_app_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dairy_app_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
